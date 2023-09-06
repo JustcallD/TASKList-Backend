7885e5d2433e84a3f6046061fa886dd98ec1303d
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-09-07 05:13:10 +0530

    timestempt added in schema

--- Models/Task.js
+++ Models/Task.js
@@ -6,16 +6,25 @@ const TaskStatus = {
   COMPLETE: "completed",
 };
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  // project: { type: mongoose.Types.ObjectId, ref: "Project" },
-  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-  status: {
-    type: String,
-    enum: TaskStatus,
-    default: TaskStatus.OPEN,
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    // project: { type: mongoose.Types.ObjectId, ref: "Project" },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: TaskStatus,
+      default: TaskStatus.OPEN,
+    },
   },
-});
+  {
+    timestamps: true, // Add createdAt and updatedAt fields
+  }
+);
 
 module.exports = mongoose.model("Tasks", taskSchema);
